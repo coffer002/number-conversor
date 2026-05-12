6c277c32da45f9795a03f814507b98896a7628f5
coffer002 <oaquila1776@gmail.com>
2026-05-12 16:42:08 -0300

    Rename bases.cpp to code_base.cpp

--- code_base.cpp
+++ code_base.cpp
@@ -1,10 +1,10 @@
 //conversor de bases <= 10 :  2, 4, 8 e 10;
 #include <stdio.h>
-//Se quiserem fazer sem separar as funcoes só substituir onde elas sao chamadas, mas ficaria com uma identação infinitamente larga;
-//Outra coisa que acho que o professor nao comentou, mas return é oq volta da funcao, por ex.: f(a, b) = c, logo se existir um return, ele vai ser c; 
+//Se quiserem fazer sem separar as funcoes sÃ³ substituir onde elas sao chamadas, mas ficaria com uma identaÃ§Ã£o infinitamente larga;
+//Outra coisa que acho que o professor nao comentou, mas return Ã© oq volta da funcao, por ex.: f(a, b) = c, logo se existir um return, ele vai ser c; 
 //Ainda sobre o return, caso seja return 0, significa sucesso e qualquer outro numero fora de contexto significa erro;
-//No caso, um dos contextos é em validacao, onde o return 1 pode ser entendido também como booleano de verdadeiro;
-//sobre nomeclatura, nome de metodos, maiusculos e minusculos, acho que tanto faz, mas usei a convenção que aprendi no Java;
+//No caso, um dos contextos Ã© em validacao, onde o return 1 pode ser entendido tambÃ©m como booleano de verdadeiro;
+//sobre nomeclatura, nome de metodos, maiusculos e minusculos, acho que tanto faz, mas usei a convenÃ§Ã£o que aprendi no Java;
 
 
 
@@ -13,15 +13,15 @@ int valido(long long num, int base){
 	
 	//permite o codigo rodar <-> a%(10^n) <= base
     for(; num > 0; num /= 10){
-    	//parametro = parametro pode ser omitido, entao só ";" como primeiro parametro da iteração funciona legal, nesse caso seria num = num, mas ele já é declarado, entao seria a toa;
-		//num /= 10 é a mesma coisa que num = num / 10, da mesma forma que qualquer outro operador (+, *, etc);
+    	//parametro = parametro pode ser omitido, entao sÃ³ ";" como primeiro parametro da iteraÃ§Ã£o funciona legal, nesse caso seria num = num, mas ele jÃ¡ Ã© declarado, entao seria a toa;
+		//num /= 10 Ã© a mesma coisa que num = num / 10, da mesma forma que qualquer outro operador (+, *, etc);
         if(num % 10 >= base) {
             return 0;}
     }
     return 1;
 }
-//long long pode ser substituído por int, double, float, short ou qualquer outro tipo de variável numérica; apenas deixei long long por armazenar valores maiores e porque, neste caso, não há problemas de memória ou tempo de processamento.
-//e n passar o n° de casas maximo; e eu ja acho pouco, queria fazer com str, mas n deixaram...
+//long long pode ser substituÃ­do por int, double, float, short ou qualquer outro tipo de variÃ¡vel numÃ©rica; apenas deixei long long por armazenar valores maiores e porque, neste caso, nÃ£o hÃ¡ problemas de memÃ³ria ou tempo de processamento.
+//e n passar o nÂ° de casas maximo; e eu ja acho pouco, queria fazer com str, mas n deixaram...
 long long paraDecimal(long long num, int origem) {
 	//retorna em base 10
     long long decimal = 0;
@@ -30,7 +30,7 @@ long long paraDecimal(long long num, int origem) {
  //Mas, declare os 2 a mesma coisa, se n vai dar pau ali em baixo por serem diferentes;
     for (; num > 0; num /= 10, potencia *= origem){
         decimal += (num % 10) * potencia;}
-// Em     num /= 10, potencia *= origem     são oq desejo que aconteça a cada loop;
+// Em     num /= 10, potencia *= origem     sÃ£o oq desejo que aconteÃ§a a cada loop;
     return decimal;
 }
 void imprimirNaBase(long long decimal, int base) {
@@ -39,7 +39,7 @@ void imprimirNaBase(long long decimal, int base) {
     // Conta quantos digitos o resultado vai ter;
     int digitos = 0;
     long long temp = decimal;
-    //temp é, nesse caso, um buf
+    //temp Ã©, nesse caso, um buf
 
     for (; temp > 0; temp /= base) digitos++;
  
@@ -51,7 +51,7 @@ void imprimirNaBase(long long decimal, int base) {
     // printa cada digito do mais significativo ao menos;
     for (int i = 0; i < digitos; i++, potencia /= base) {
         printf("%lld", decimal / potencia);
-        //acho que nem precisava deixar isso comentado, mas enfim, %lld é para long, use %d se declarou anteriormente como int ;
+        //acho que nem precisava deixar isso comentado, mas enfim, %lld Ã© para long, use %d se declarou anteriormente como int ;
         decimal %= potencia;}
 }
 int main() {
